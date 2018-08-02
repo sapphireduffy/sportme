afe3e67ccd4807e4b3c7e1eae3a012627efbfe1b
Sapphire Duffy <sapphireduffy@gmail.com>
2018-08-02 21:02:08 +0100

    Example commit

--- SportMe/ViewController.swift
+++ SportMe/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Opened main login screen")
     }
 
     override func didReceiveMemoryWarning() {
